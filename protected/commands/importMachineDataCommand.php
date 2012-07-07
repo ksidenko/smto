2649d3825adf3f359be36c98524e5989fdc49f8c
sks <sks.develop@gmail.com>
2012-07-07 14:39:19 +0700

    add yii

---
+++ protected/commands/importMachineDataCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kirill
+ * Date: 7/7/12
+ * Time: 2:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ImportMachineDataCommand extends CConsoleCommand {
+
+    public function run($args) {
+        echo '!!!!!!!!!';
+    }
+}
